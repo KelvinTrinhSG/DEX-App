f6c405f4b20685e0ddc3acfff3b69c3f013b679a
KelvinTrinh <thkien85@Gmail.com>
2024-05-03 11:58:19 +0700

    Initialize App
    Initialize App

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,39 @@
 import type { AppProps } from "next/app";
 import { ThirdwebProvider } from "@thirdweb-dev/react";
 import "../styles/globals.css";
+import Navbar from "../components/Navbar";
+
 
 // This is the chain your dApp will work on.
 // Change this to the chain your app is built for.
 // You can also import additional chains from `@thirdweb-dev/chains` and pass them directly.
-const activeChain = "ethereum";
+// const activeChain = "opBNB";
+const customChain = {
+  // Required information for connecting to the network
+  chainId: 204, // Chain ID of the network
+  rpc: ["https://opbnb-mainnet-rpc.bnbchain.org"], // Array of RPC URLs to use
+ 
+  // Information for adding the network to your wallet (how it will appear for first time users) === \\
+  // Information about the chain's native currency (i.e. the currency that is used to pay for gas)
+  nativeCurrency: {
+    decimals: 18,
+    name: "opBNB Mainnet",
+    symbol: "BNB",
+  },
+  shortName: "opBNB", // Display value shown in the wallet UI
+  slug: "opBNB", // Display value shown in the wallet UI
+  testnet: false, // Boolean indicating whether the chain is a testnet or mainnet
+  chain: "opBNB", // Name of the network
+  name: "opBNB Mainnet", // Name of the network
+};
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThirdwebProvider
       clientId={process.env.NEXT_PUBLIC_TEMPLATE_CLIENT_ID}
-      activeChain={activeChain}
+      activeChain={customChain}
     >
+      <Navbar />
       <Component {...pageProps} />
     </ThirdwebProvider>
   );
